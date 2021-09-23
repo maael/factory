670e2bd326df3aa5a726c53fb2f5a54eb1d989c1
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-09-23 08:19:20 +0100

    fix: 🐛 Order scenes for loading correctly

--- src/game/index.ts
+++ src/game/index.ts
@@ -2,13 +2,15 @@ import * as Phaser from 'phaser'
 import { log } from '~/game/util/log'
 import * as Scenes from '~/game/scenes'
 
+const orderedScenes = Object.values(Scenes).sort((a, b) => ((a as any).isStart ? -1 : (b as any).isStart ? 1 : 0))
+
 export default function init(canvas: HTMLCanvasElement) {
   const config: Phaser.Types.Core.GameConfig = {
     canvas,
     type: Phaser.CANVAS,
     width: document.body.clientWidth,
     height: document.body.clientHeight,
-    scene: Object.values(Scenes).sort((a) => ((a as any).isStart ? -1 : (a as any).isStart ? 1 : 0)),
+    scene: orderedScenes,
     banner: false,
   }
   log('initialising game')
