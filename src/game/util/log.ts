9c27df9d09c621bdeb1857f2f98b0c1ba603b129
Matthew Elphick <matthew.elphick@threadsstyling.com>
2021-09-23 08:17:37 +0100

    fix: 🐛 Make logger work on mac

--- src/game/util/log.ts
+++ src/game/util/log.ts
@@ -5,7 +5,8 @@ export function log(...args: any[]) {
     .split('@')[0]
     .trim()
     .split(' ')
-  let initial = parts ? parts[1].trim() : undefined
-  if (initial === 'new') initial = parts ? `${parts[2].trim()}.constructor` : undefined
+  let initial = parts && parts[1] ? parts[1].trim() : undefined
+  if (initial === 'new') initial = parts && parts[2] ? `${parts[2].trim()}.constructor` : undefined
+  if (!initial) initial = parts && parts[0] ? parts[0].trim() : 'Unknown'
   console.info(`[${initial}]`, ...args)
 }
